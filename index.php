57806d4891d56ef4c34a7eef06ecd09a9f08e242
Super-Michael-05 <mikaelkolehmainen05@gmail.com>
2021-12-15 10:04:41 +0200

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,167 @@
-<?php 
-    echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ruosteinen Rauta Oy</title>
+        <!-- Favicon -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="main.css" rel="stylesheet" type="text/css">
+        <!-- Scripts -->
+    </head>
+    <body>
+        <header>
+            <img src="img/logo/Logo dark.png" alt="Company logo">
+            <nav id="navbar">
+                <ul>
+                    <a href="index.php"><li>Etusivu</li></a>
+                    <a href="template.php"><li>Esimerkkisivu</li></a>
+                    <a href="login.php"><li>Kirjaudu</li></a>
+                </ul>
+            </nav>
+        </header>
+        <div class="hero-background">
+            <div class="hero-text">
+                <h1>Oikein rakentaminen onnistuu hyvillä ratkaisuilla - Tervetuloa asioimaan Ruosteiseen Rautaan!</h1>
+                <button>Lue lisää</button>
+            </div>
+        </div>
+        <section>
+            <!-- Yritysesittely -->
+            <article>
+                <h2>Meistä</h2>
+                <p>Ruosteinen Rauta Oy on perustettu 1958. Myymälästämme löydät erittäin laadukkaita rauta ja metallialan tarvikkeita sekä palveluita kaikkeen rakentamiseen – oli kyse sitten korjaamisesta, remontoimisesta tai uuden rakentamisesta suurissa tai pienissä kohteissa. Meillä asiat hoituvat tutulla ammattitaitoisella tavalla alusta loppuun ja kokeneet ammattilaiset auttavat sinua löytämään parhaat ratkaisut, rakennusmateriaalit tuotteet sekä koneet ja vuokralaitteet.</p>
+                <p>Myymälämme valikoimasta saat kaiken yhdellä kertaa suuriin ja pieniin rakennus-, remontointi- ja sisustusprojekteihin ulos ja sisälle. Yrityksemme arvot ovat kestävän kehitys, osaava ja hyvinvoiva henkilökunta sekä työturvallisuus.</p>
+                <p>Oikein rakentaminen onnistuu hyvillä ratkaisuilla - Tervetuloa asioimaan Ruosteiseen Rautaan!</p>
+                <p>Ruosteinen Rauta on Porin Yrittäjien jäsenyritys. Tutustu Porin Yrittäjien toimintaan: <a href="https://www.yrittajat.fi/paikallisyhdistykset/porin-yrittajat/" target="_blank">https://www.yrittajat.fi/paikallisyhdistykset/porin-yrittajat/</a></p>
+                <h2>Historia</h2>
+                <p>Vuonna 1958 avattiin ensimmäisen kerran Ruosteinen Rauta Oy:n myymälä Porin kauppatorille, osoitteeseen Pohjoiskauppatori, 28100 Pori. Tässä osoitteessa myymämme toimi aina vuoteen 2020 asti palvellen ensimmäisenä rautakauppana Porilaisia. Myymälämme on toiminut vuodesta 2020 alkaen osoitteessa Urheilutie 1, 28500 Pori, jolloin avasimme supermyymälän – joka on yksi pohjoismaiden suurimpia rauta ja metallialan myymälöitä. Vuonna 2020 laajensimme palveluihimme myös kone ja laitteistovuokrauksen kaikkeen rakentamiseen.</p>
+                <p>Yrityksen perustaja Rauno Rauta on palvellut asiakkaita vuodesta 1958 ja vuonna 2022 Tuukka Rauta jatkaa yrityksen ohjaksissa Reino Raudan jäädessä eläkkeelle. Tuukka Rauta on toiminut yrityksessä jo vuodesta 2000.  Vuonna 2021 yrityksessämme työskenteli 100 alan ammattilaista palvellen asiakkaitamme rautaisella ammattitaidolla.</p>
+                <p>Yrityksemme ideana on alusta lähtien tarjota rakennus ja metallialan tuotteita ja palveluita edulliseen hintaan. Palveluumme on aina kuulunut ammattitaitoinen opastus ja asiantuntemus, jota asiakkaamme useiden sukupolvien ajan ovat hyödyntäneet projekteissaan. Voimmekin rehellisesti sanoa, että tunnemme alan läpikotaisin ja tämän pitkän kokemuksen tuomalla varmuudella pystymme tarjoamaan asiakkaillemme kaikki rauta ja metallialanpalvelut luotettavasti ja laadukkaasti – jo vuodesta 1958.</p>
+            </article>
+            <!-- Referenssejä -->
+            <article>
+                <!-- Referenssi 1 -->
+                <div id="reference1">
+                    <h2>Suurhanke: Helsinki – Tallinna Silta / Valtakunnan Silta Oy</h2>
+                    <img src="img/referenssi1.jpg" alt="kuva sillasta">
+                    <p>Massiivinen, reippaan punainen Tallinnan silta avattiin liikenteelle tammikuussa 2020. Siltahanke on yksi Suomen ja Viron suurimpia yhteisiä rakennushankkeita. Tähän merkittävään rakennushankkeeseen osallistuivat kaikki suurimmat rakennusyhtiöt ja toimijat Suomesta ja Virosta.  Ruosteinen Rauta Oy: toimitti kaikki sillan kaiteitten terärakenteiden tukevat pultit ja kaapelien suojaputket pääurakoitsija Valtakunnan Sillat Oy:lle. Pultti ja suojaputkitoimitukset saatiin toimitettua aikataulun mukaisesti.</p>
+                    <p>Valtakunnan Sillat Oy valitsi Ruosteinen Rauta Oy:n pultti ja suojaputkitoimittajaksi aikaisemman yhteistyön perusteella luottaen, että hankkeen tiukassa aikataulussa pysytään, kun pultit ja suojaputkitoimituksissa on luotettava kumppani.</p>
+                    <p>Seppo Silta kertookin yhteistyöstä ja toimituksista seuraavasti ”Ruosteinen Rauta yhtenä suurimpana rautakauppa ja metallialan toimittajana pystyy toimittamaan suuria määriä rakennustarvikkeita kriittisiin projekteihin. Yhteistyö Ruosteisen Raudan kanssa on toiminut aina hyvin ja tehokkaasti”.</p>
+                </div>
+                <!-- Referenssi 2 -->
+                <div id="reference2">
+                    <h2>Konepalvelut vuokraus / Koneline Oy</h2>
+                    <img src="img/referenssi2.webp" alt="kuva kaivurista">
+                    <p>Koneline Oy tarjoaa maansiirto ja maatyö palveluita rakentajille. Vuonna 2021 KoneLine Oy luopui omista maansiirtokoneistaan ja käyttää urakoissaan Ruosteinen Rauta Oy:n vuokralaitevalikoimaa.</p>
+                    <p>”Ruosteinen Rauta Oy” toimittaa meille projekteihin tarvittavia laitteistoja ja koneita. Koneet ja laitteet ovat aina toimivia ja hyvin huollettuja. Siirtyessämme vuokrakoneiden käyttöön, olemme pystyneet tarjoamaan maansiirtotöitä erittäin edulliseen hintaan asiakkaillemme. Vuokrakoneiden käytössä pystymme käyttämään ja saamaan käyttöömme juuri oikean laitteen oikeaan aikaan ja tämä luo liiketoimintaan kustannustehokkuutta. Ruosteinen Rauta on merkittävä kumppanimme, joka mahdollistaa toimintamme joustavuuden. Kannattavuutemme on kasvanut 89% kun siirryimme hyödyntämään Ruosteisen Raudan vuokrakonevalikoimaa toiminnassamme. ” Kertoo toimitusjohtaja Jorma Kontio Koneline Oy:stä. Koneline Oy suositteleekin Ruosteisen Raudan konevalikoimaan pienemmille ja suuremmille työmaille.</p>
+                </div>
+                <!-- Referenssi 3 -->
+                <div id="reference3">
+                    <h2>Talonrakentaja Olli Orava</h2>
+                    <img src="img/referenssi3.jpg" alt="kuva talosta">
+                    <p>Uniikki koti rakentui Poriin vuonna 2019. Rakentaja Jussi Ahola rakensi unelmiensa kodin hankkien kaikki rakennustarvikkeet Ruosteisen Raudan monipuolisesta valikoimasta. Ahola on 12 metriä korkea moniulotteinen rakennus, oikea taideteos, joka toteutettiin Arkkitehtitoimisto Ainutlaatuinen koti Oy:n suunnitelmien pohjalta.</p>
+                    <p>Jussi Ahola kommentoi rakennusprojektiaan seuraavasti: ”Rakentaminen oli helppoa ja nopeaa Ruosteisen Raudan avulla. Monipuolinen valikoima mahdollisti kaikkien tarvikkeiden hankinnan yhdestä ja samasta paikasta, jolloin säästin kustannuksista ja sain aina kokonaisvaltaista palvelua kaikkiin rakentamiseen liittyviin kysymyksiin ja palveluihin liittyen. Suosittelen Ruosteisen Raudan valikoimaa lämpimästi kaikille rakentajille pieniin ja isoihin projekteihin.”</p>
+                </div>
+            </article>
+            <!-- Palvelut -->
+            <article>
+                <h2>Palvelut</h2>
+                <!-- Palvelu 1 -->
+                <div id="service1">
+                    <h2>Rakennustarvikkeet</h2>
+                    <p>Ruosteinen Rauta tarjoaa rakennustarvikkeet alan johtavilta merkeiltä nopeasti toimitettuna suoraan työmaalle. Ruosteisen Raudan Rakennustarvikkeet – palvelun avulla rakentaminen on helppoa ja edullista. Laadukkuus ja edullisuus ovat ominaisuuksia, joita ostajat odottavat rakennustarvikkeilta. Rakennustarvikkeet - valikoimasta löytyy paljon vaihtoehtoja pienempiin tai isompiin rakennus- tai remontointiprojekteihin.</p>
+                    <ul>
+                        <li>Perustustarvikkeet</li>
+                        <li>Rakennusmateriaalit</li>
+                        <li>Sähkötarvikkeet</li>
+                        <li>Ilmanvaihto</li>
+                        <li>Lämmitys</li>
+                        <li>LVI</li>
+                        <li>Työkalut ja tarvikkeet</li>
+                    </ul>
+                </div>
+                <!-- Palvelu 2 -->
+                <div id="service2">
+                    <h2>Sisustustarvikkeet</h2>
+                    <p>Kun talo on rakennettu, voidaan keskittyä sisustamiseen. Me autamme sinua myös kaikissa sisustamiseen liittyvissä ratkaisuissa. Valikoimaamme kuuluu kaikki sisustukseen tarvittavat tuotteet ja ratkaisut, kuten keittiökalusteet, takat, tapetit, lattiapinnoitteet, laatat, koukut ja maalit.</p>
+                    <ul>
+                        <li>Keittiö</li>
+                        <li>Kylpyhuone ja wc</li>
+                        <li>Laatat | lattialaatat, seinälaatat, kuviolaatat</li>
+                        <li>Lattiat | parketit, laminaatit, korkkilattiat, vinyylilattiat</li>
+                        <li>Ovet</li>
+                        <li>Kiukaat</li>
+                        <li>Tapetit</li>
+                        <li>Koukut naulakot ja hyllyt</li>
+                        <li>Takat</li>
+                        <li>Valaistus</li>
+                    </ul>
+                </div>
+                <!-- Palvelu 3 -->
+                <div id="service3">
+                    <h2>Piharakennus</h2>
+                    <p>Laajasta valikoimastamme löydät kaiken tarvittavan pihan suunnitteluun ja rakentamiseen.</p>
+                    <ul>
+                        <li>Pihakivetys ja päällysteet</li>
+                        <li>Puutarhakalusteet ja tarvikkeet</li>
+                        <li>Kylpytynnyrit ja uima-altaat</li>
+                        <li>Pihavalaisimet</li>
+                    </ul>
+                </div>
+                <!-- Palvelu 4 -->
+                <div id="service4">
+                    <h2>Konevuokraus</h2>
+                    <p>Vuokraamme laadukkaat ja turvalliset koneet erikokoisiin projekteihin yrityksille, yhteistyökumppaneille ja yksityisille rakentajille ja remontoijille. Varaa ja tilaa tarvitsemasi kone tai laite, kuljetamme koneen kohteeseen sovittuna ajankohtana. Esimerkkejä koneista ja laitteistamme:</p>
+                    <ul>
+                        <li>Henkilönostimet</li>
+                        <li>Nosto- ja siirtokalusto</li>
+                        <li>Maantiivistyskalusto</li>
+                        <li>Maansiirtokalusto</li>
+                        <li>Peräkärryt</li>
+                        <li>Avant</li>
+                        <li>Generaattorit</li>
+                    </ul>
+                </div>
+            </article>
+        </section>
+        <!-- Yhteystiedot & Yhteydenottolomake -->
+        <footer>
+            <h1>Ota yhteyttä</h1>
+            <!-- Yhteystiedot -->
+            <div id="rest">
+                <img src="img/logo/Logo dark.png" alt="Company logo">
+                <h3>Ruosteinen Rauta Oy</h3>
+                <p>Urheilutie 1,</p>
+                <p>28500 Pori</p>
+                <p>Puh. 044 7011442</p>
+                <p>info@ruosteinenrauta.fi</p>
+            </div>
+            <!-- Yhteydenottolomake -->
+            <aside>
+                <form id="contact-us" name="contact-us" action="" method="POST" autocomplete="off">
+                    <div class="floating-label-group">
+                        <input type="text" id="fname" name="fname" class="form-control" autocomplete="off" autofocus required />
+                        <label class="floating-label">Etunimi</label>
+                    </div>
+                    <div class="floating-label-group">
+                        <input type="text" id="lname" name="lname" class="form-control" autocomplete="off" autofocus required />
+                        <label class="floating-label">Sukunimi</label>
+                    </div>
+                    <div class="floating-label-group">
+                        <input type="text" id="phone" name="phone" class="form-control" autocomplete="off" autofocus required />
+                        <label class="floating-label">Puhelinnumero</label>
+                    </div>
+                    <div class="floating-label-group">
+                        <input type="text" id="e-mail" name="e-mail" class="form-control" autocomplete="off" autofocus required />
+                        <label class="floating-label">Sähköposti</label>
+                    </div>
+                    <div class="floating-label-group">
+                        <input type="text" id="msg" name="msg" class="form-control" autocomplete="off" autofocus required />
+                        <label class="floating-label">Viesti</label>
+                    </div>
+                    <input id="confirmation" type="submit" value="Lähetä">
+                </form>
+            </aside>
+            <p>Mikael Kolehmainen &copy; 2022</p>
+        </footer>
+    </body>
+</html>
