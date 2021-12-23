5488249f15cc33db9765e999f17c11db88cfe15d
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2021-12-23 08:36:44 +0200

    Update 23.12.2021 8:36

--- login.php
+++ login.php
@@ -6,6 +6,19 @@
     </head>
     <?php require 'onload.php'?>
         <?php require 'header.php'?>
+        <section id="login-page">
+                <form id="user" name="user" action="user-page.php" method="POST" autocomplete="off">
+                    <div class="floating-label-group">
+                        <input type="text" id="username" name="username" class="form-control" autocomplete="off" required />
+                        <label class="floating-label">Käyttäjänimi</label>
+                    </div>
+                    <div class="floating-label-group">
+                        <input type="password" id="pw1" name="pw1" class="form-control" autocomplete="off" required/>
+                        <label class="floating-label">Salasana</label>
+                    </div>
+                    <input id="sign-in" type="submit" value="Kirjaudu">
+                </form>
+        </section>
         <!-- Yhteystiedot & Yhteydenottolomake -->
         <?php require 'footer.php'?>
     </body>
