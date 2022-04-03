ad4552302af0bbdac7a8324d7fd9255c6279ecc8
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2022-04-03 18:08:09 +0300

    Update 03.04.2022 18:08

--- add-page.php
+++ add-page.php
@@ -6,28 +6,41 @@
         $fh = fopen($myFile, "w") or exit("Error creation");
         // Save data to database
         require 'connection.php';
+        session_start();
 
+        $siteName = $_POST['sitetitle'];
+        $siteTitle = $_POST['sitetitle'];
+        $siteImage = 'img/unsplash_images/'.$_POST['siteimage'];
+        $contentIDs = $_SESSION['contentAmount'];
+        
+        $sql = "INSERT INTO sivut (nimi, nimiurl, otsikko, teemakuva, sisalto) 
+                    VALUES ('$siteName', '$myFile', '$siteTitle', 
+                    '$siteImage', '$contentIDs')";
+        mysqli_query($conn, $sql);
 
-
-
+       // $contentdb = "SELECT id, alaotsikko, tekstit, kuvat FROM sisalto";
         // save php code to $content as in echo the code, the variables from database
         // Write html to html file
-        $content = "
+        $content = '
             <!DOCTYPE html>
-            <?php require 'html-tag.php' ?>
+            <?php
+                require "connection.php";
+                $sql = "SELECT id, nimi, nimiurl, otsikko, teemakuva, sisalto FROM sivut";
+                $getData = mysqli_query($conn, $sql);
+            ?>
+            <?php require "html-tag.php" ?>
                 <head>
-                    <title>Ruosteinen Rauta Oy - {$_POST['sitetitle']}</title>
-                    <meta name='robots' content='index, follow'>
-                    <?php require 'head.php'?>
+                    <?php echo "<title>Ruosteinen Rauta Oy - {}</title>"; ?>
+                    <meta name="robots" content="index, follow">
+                    <?php require "head.php"?>
                 </head>
                 <body>
-                    <?php require 'header.php'?>
-                    <div id='feature-img' style='background-image: url(img/unsplash_images/{$_POST['siteimage']})'>
-                        <h1>{$_POST['sitetitle']}</h1>
+                    <?php require "header.php"?>
+                    <div id="feature-img" style="background-image: url()">
+                        <h1>{}</h1>
                     </div>
                     <section>
-        ";
-        session_start();
+        ';
         for ($i = 1; $i <= $_SESSION['contentAmount']; $i++) {
             $subtitle = $_POST["subtitle$i"];
             $subtext = $_POST["subtext$i"];
--- testi.php
+++ testi.php
@@ -1,45 +1,30 @@
 
             <!DOCTYPE html>
-            <?php require 'html-tag.php' ?>
+            <?php
+                require "connection.php";
+                $sql = "SELECT id, nimi, nimiurl, otsikko, teemakuva, sisalto FROM sivut";
+                $getData = mysqli_query($conn, $sql);
+            ?>
+            <?php require "html-tag.php" ?>
                 <head>
-                    <title>Ruosteinen Rauta Oy - testi</title>
-                    <meta name='robots' content='index, follow'>
-                    <?php require 'head.php'?>
+                    <?php echo "<title>Ruosteinen Rauta Oy - {}</title>"; ?>
+                    <meta name="robots" content="index, follow">
+                    <?php require "head.php"?>
                 </head>
                 <body>
-                    <?php require 'header.php'?>
-                    <div id='feature-img' style='background-image: url(img/unsplash_images/neonbrand-60krlMMeWxU-unsplash.jpg)'>
-                        <h1>testi</h1>
+                    <?php require "header.php"?>
+                    <div id="feature-img" style="background-image: url()">
+                        <h1>{}</h1>
                     </div>
                     <section>
         
                         <article>
                             <div class='row'>
                                 <div class='left'>
-                                    <h1>moro</h1>
-                                    <p>lafkfje</p>
+                                    <h1>testi</h1>
+                                    <p>testapfkjakfne</p>
                                 </div>
                                 <aside class='rightImg'><img src='img/unsplash_images/matt-artz-pH6wLT6TVFc-unsplash.jpg' alt=''></aside>
-                            </div>
-                        </article>
-                
-                        <article>
-                            <div class='row'>
-                                <div class='right'>
-                                    <h1>aefegeg</h1>
-                                    <p>gegegge</p>
-                                </div>
-                                <aside class='leftImg'><img src='img/unsplash_images/matt-artz-pH6wLT6TVFc-unsplash.jpg' alt=''></aside>
-                            </div>
-                        </article>
-                
-                        <article>
-                            <div class='row'>
-                                <div class='left'>
-                                    <h1>eheheh</h1>
-                                    <p>ehehteg</p>
-                                </div>
-                                <aside class='rightImg'><img src='img/unsplash_images/mika-baumeister-c71yQepDEjo-unsplash.jpg' alt=''></aside>
                             </div>
                         </article>
                 
